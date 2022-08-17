78a2d701d687c3507f2296f27813a19895382a51
miker-bice <mikejerrson.galindez@gmail.com>
2022-08-17 13:56:20 +0800

    animal character added | index page almost done

--- index.php
+++ index.php
@@ -7,9 +7,15 @@
     include 'includes/navbar.php';
 ?>
 
-<div class="main-content container text-center py-4">
-    <h1>this is the home content</h1>
-    <a href="" class="btn btn-primary">Click here</a>
+<div class="main-content container text-center">
+    
+        <img class="img-fluid" width="300" src="images/Monkey.svg" alt="main-character">
+        <h1>Hello, Name!</h1>
+        <div class="buttons-container d-grid container-fluid w-50">
+            <a href="" class="btn btn-success my-2">Browse Lessons</a>
+            <a href="" class="btn btn-outline-success my-2">Browse Quizzes</a>
+        </div>
+    
 </div>
 
 <?php 
